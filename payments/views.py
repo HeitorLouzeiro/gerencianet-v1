37a44a893fb1bf13925cc57088965d668551582f
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-30 15:48:49 -0300

    Generating billet with trial balance in the Gerencianet API.

--- payments/views.py
+++ payments/views.py
@@ -10,28 +10,86 @@ gn = Gerencianet(CREDENTIALS)
 def home(request):
     body = {
         'items': [{
-            'name': "Couse 1",
+            'name': "Product 1",
             'value': 1000,
             'amount': 2
         }],
         'shippings': [{
             'name': "Default Shipping Cost",
             'value': 100
-        }],
-        'payment': {
-            'banking_billet': {
-                'expire_at': '2023-01-01',
-                'customer': {
-                    'name': "Gorbadoc Oldbuck",
-                    'email': "heitorlouzeiro2019@gmail.com",
-                    'cpf': "14014603059",
-                    'birth': "1977-01-15",
-                    'phone_number': "62986070247"
-
-                }
-            }
-        }
+        }]
     }
-    response = gn.create_charge_onestep(params=None, body=body)
+    response = gn.create_charge(body=body)
+    transaction_id = response['data']['charge_id']
+
+    params = {
+        'id': transaction_id
+    }
+    body = {
+        'title': 'Balancete Demonstrativo',
+        'body': [
+            {
+                'header': 'Demonstrativo de Consumo',
+                'tables': [
+                    {
+                        'rows': [
+                            [
+                                {
+                                    'align': 'left',
+                                    'color': '#000000',
+                                    'style': 'bold',
+                                    'text': 'Exemplo de despesa',
+                                    'colspan': 2
+                                },
+                                {
+                                    'align': 'left',
+                                    'color': '#000000',
+                                    'style': 'bold',
+                                    'text': 'Total lançado',
+                                    'colspan': 2
+                                }
+                            ],
+                            [
+                                {
+                                    'align': 'left',
+                                    'color': '#000000',
+                                    'style': 'normal',
+                                    'text': 'Instalação',
+                                    'colspan': 2
+                                },
+                                {
+                                    'align': 'left',
+                                    'color': '#000000',
+                                    'style': 'normal',
+                                    'text': 'R$ 100,00',
+                                    'colspan': 2
+                                }
+                            ]
+                        ]
+                    }
+                ]
+            },
+            {
+                'header': 'Balancete Geral',
+                'tables': [
+                    {
+                        'rows': [
+                            [
+                                {
+                                    'align': 'left',
+                                    'color': '#000000',
+                                    'style': 'normal',
+                                    'text': 'Confira na documentação da Gerencianet todas as configurações possíveis.',
+                                    'colspan': 4
+                                }
+                            ]
+                        ]
+                    }
+                ]
+            }
+        ]
+    }
+
+    response = gn.create_charge_balance_sheet(params=params, body=body)
     print(response)
     return render(request, 'payments/pages/home.html')
