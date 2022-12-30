cfd1b6b220ec1f94c78b48dbcec173995e108109
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-30 14:52:55 -0300

    Generating a transaction in Gerencianet.

--- credentials/credentials.py
+++ credentials/credentials.py
@@ -4,5 +4,5 @@ CREDENTIALS = {
     "client_id": os.environ.get('CLIENT_ID'),
     "client_secret": os.environ.get('CLIENT_SECRET'),
     'sandbox': True,
-    'certificate': 'credentials/certificate/homologacao-429610-certficate.pem'
+    # 'certificate': 'credentials/certificate/homologacao-429610-certficate.pem'
 }
--- payments/views.py
+++ payments/views.py
@@ -9,20 +9,17 @@ gn = Gerencianet(CREDENTIALS)
 
 def home(request):
     body = {
-        'calendario': {
-            'expiracao': 3600
-        },
-        'devedor': {
-            'cpf': '12345678909',
-            'nome': 'Francisco da Silva'
-        },
-        'valor': {
-            'original': '123.45'
-        },
-        'chave': '2f2924f3-78f3-4864-8e84-ee083c4f5bb0',
-        'solicitacaoPagador': 'Cobrança dos serviços prestados.'
+        'items': [{
+            'name': "Product 1",
+            'value': 1000,
+            'amount': 2
+        }],
+        'shippings': [{
+            'name': "Default Shipping Cost",
+            'value': 100
+        }]
     }
 
-    response = gn.pix_create_immediate_charge(body=body)
+    response = gn.create_charge(body=body)
     print(response)
     return render(request, 'payments/pages/home.html')
