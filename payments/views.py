14b404e24c9c95abedcdba821e753f4ab64d9d90
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-30 17:32:57 -0300

    Generating payment slips in the Gerencianet API.

--- payments/views.py
+++ payments/views.py
@@ -8,41 +8,22 @@ gn = Gerencianet(CREDENTIALS)
 
 
 def home(request):
-    # Creating a plan in Gerencianet
-    # body = {
-    #     'name': "Meu Plano de Assinatura",
-    #     'repeats': None,
-    #     'interval': 1
-    # }
-
-    # response = gn.create_plan(params=None, body=body)
-    # print(response)
-
-    # id plan
-    params = {
-        'id': 9935
-    }
-
     body = {
         'items': [{
-            'name': "Product 1",
+            'name': 'Carnet Item 1',
             'value': 1000,
             'amount': 2
         }],
-        'payment': {
-            'banking_billet': {
-                'expire_at': '2023-01-31',
-                'customer': {
-                    'name': "Gorbadoc Oldbuck",
-                    'email': "heitorlouzeiro2019@gmail.com",
-                    'cpf': "94271564656",
-                    'birth': "1977-01-15",
-                    'phone_number': "5144916523"
-                }
-            }
-        }
+        'customer': {
+            'name': 'Gorbadoc Oldbuck',
+            'email': 'oldbuck@gerencianet.com.br',
+            'cpf': '94271564656',
+            'birth': '1977-01-15',
+            'phone_number': '5144916523'
+        },
+        'repeats': 3,
+        'expire_at': '2023-01-01'
     }
-
-    response = gn.one_step_subscription(params=params, body=body)
+    response = gn.create_carnet(body=body)
     print(response)
     return render(request, 'payments/pages/home.html')
