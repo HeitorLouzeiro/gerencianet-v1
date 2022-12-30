9661e4f2957a868f3163801c7ce841e708ce064a
Heitor Louzeiro <heitorlouzeiro2019@gmail.com>
2022-12-30 14:57:13 -0300

    Link to payment method via bank slip.

--- payments/views.py
+++ payments/views.py
@@ -8,18 +8,24 @@ gn = Gerencianet(CREDENTIALS)
 
 
 def home(request):
+    params = {
+        'id': 1785912
+    }
     body = {
-        'items': [{
-            'name': "Product 1",
-            'value': 1000,
-            'amount': 2
-        }],
-        'shippings': [{
-            'name': "Default Shipping Cost",
-            'value': 100
-        }]
+        'payment': {
+            'banking_billet': {
+                'expire_at': '2022-12-31',
+                'customer': {
+                    'name': "Gorbadoc Oldbuck",
+                    'email': "oldbuck@gerencianet.com.br",
+                    'cpf': "94271564656",
+                    'birth': "1977-01-15",
+                    'phone_number': "5144916523"
+                }
+            }
+        }
     }
 
-    response = gn.create_charge(body=body)
+    response = gn.pay_charge(params=params, body=body)
     print(response)
     return render(request, 'payments/pages/home.html')
